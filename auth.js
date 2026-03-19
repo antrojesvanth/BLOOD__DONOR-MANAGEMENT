1aba2cbc1bb0fd877189d003e9fe2dd1c2c6ca38
ANTRO JESVANTH V <antrojesvanth.ec24@krct.ac.in>
2026-03-19 19:51:29 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-import { auth, db } from './firebase-config.js';
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
